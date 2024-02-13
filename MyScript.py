9bb013ecdb84db7beadd65d6a31de00b9feec26e
Carmenciubancan <carmenciubancan7@gmail.com>
2024-02-14 00:46:42 +0100

    started script

---
+++ MyScript.py
@@ -0,0 +1,16 @@
+print('Hello, Bobi, what are you focusing on today?')
+
+
+task1 = input('....')
+print(task1)
+task2 = input('....')
+print(task2)
+task3 = input('....')
+print(task3)
+task4 = input('....')
+print(task4)
+
+
+
+
+
